c323d78d021de4c450f7a118eff16d4e45358e1d
mish524 <mishane1998@gmail.com>
2020-04-21 15:10:28 +0530

    copy and pasted with master

---
+++ app/src/main/java/com/example/boadme/displayDetailsB.java
@@ -0,0 +1,15 @@
+package com.example.boadme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class displayDetailsB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_details_b);
+    }
+}
